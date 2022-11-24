7639f571c13a65530b9604160532b1f786aa85c6
Minjur <minjur.rahaman99@gmail.com>
2022-11-24 12:40:49 +0600

    Banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,8 +6,8 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content lg:py-32">
                 <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <h1 className="mb-5 text-5xl font-bold">Buy Used Microbus, Luxury and Electic Cars</h1>
+                <p className="mb-5">We are here to selling used cars, Like as Microbus, Luxury and Electric cars. With 55,000+ cars it's okey to be picky. You can also sell your one here..</p>
                 <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
